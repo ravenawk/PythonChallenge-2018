33d6f3c64a99c4bf34dc4d8669a31db214b4785f
ravenawk <23747724+ravenawk@users.noreply.github.com>
2018-01-16 08:40:25 -0800

    Fully working - not able to pass in ips yet

--- 02-week/ipinfo.py
+++ 02-week/ipinfo.py
@@ -3,19 +3,31 @@
 
 import json
 import requests
+# from pprint import pprint
 
-website = "http://ip-api.com/json"
+website = "http://ip-api.com/json/8.8.8.8"
 
 
 def getipinfo(website):
-    "Get ip info from site"
-    ipinfo = json.loads(requests.get(website))
-    print(ipinfo)
+    """Get ip info from website."""
+    inforequest = requests.get(website)
+    infobyte = inforequest.content
+    return json.loads(infobyte)
+
+
+def printinfo(ipinfo):
+    """Print out the Ip info."""
+    print("IP : {}".format(ipinfo['query']))
+    print("ISP: {}".format(ipinfo['isp']))
+    print("City: {}".format(ipinfo['city']))
+    print("Country: {}".format(ipinfo['country']))
+    print("Country Code: {}".format(ipinfo['countryCode']))
 
 
 def Main():
-    "Run if script is run."
-    getipinfo(website)
+    """Run if script is run."""
+    ipinfo = getipinfo(website)
+    printinfo(ipinfo)
 
 
 if __name__ == "__main__":

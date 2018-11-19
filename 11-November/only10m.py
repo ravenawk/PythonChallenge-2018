a041e0ecbdf929e3777f88e9c343def510ef27cb
Pat Martin <wpmartin@Pats-MacBook-Air.local>
2018-11-19 11:22:54 -0800

    Rework of only10m.py

--- 11-November/only10m.py
+++ 11-November/only10m.py
@@ -1,26 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+"""Pull a cisco port config."""
 
+from netmiko import ConnectHandler
+import getpass
+import argparse
 import re
 
 
-message = r"""Gi0/2                        connected    16         a-full a-1000 10/100/1000BaseTX
-Gi0/3                        connected    206        a-half   a-10 10/100/1000BaseTX
-Gi0/4                        connected    16         a-full a-1000 10/100/1000BaseTX
-Gi0/5                        connected    16         a-full  a-100 10/100/1000BaseTX
-Gi0/6                        connected    16         a-full a-1000 10/100/1000BaseTX
-Gi0/7                        notconnect   3            auto   auto 10/100/1000BaseTX
-Gi0/8                        connected    16         a-full a-1000 10/100/1000BaseTX
-Gi0/9                        connected    16         a-full a-1000 10/100/1000BaseTX
-Gi0/10                       connected    26         a-full a-1000 10/100/1000BaseTX
-Gi0/11                       connected    16         a-full a-1000 10/100/1000BaseTX"""
+def Main():
+    """Run if run as a program."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--device_os", type=str, default="cisco_ios",
+                        help='Device OS default=Cisco IOS', metavar="")
+    parser.add_argument("-n", "--hostname", required=True,
+                        help='Hostname of device', metavar="")
+    parser.add_argument("-u", "--user", type=str, default="admin",
+                        help='user to connect with default=admin', metavar="")
+    args = parser.parse_args()
 
-newmessage = message.split("\n")
-for i in newmessage:
-    tenmeg = re.compile(r'a-10\s')
-    mo = tenmeg.search(i)
-    if mo != None:
-        print(f"Another - {i}")
+    password = getpass.getpass()
+    hostname = args.hostname + ".ne.pvhmc.org"
+    cisco = {'device_type': args.device_os,
+             'ip': hostname,
+             'username': args.user,
+             'password': password,
+             }
 
-tenmeg = re.compile(r'a-10\s')
-mo = tenmeg.search(message)
-print(mo.group())
+    net_connect = ConnectHandler(**cisco)
+    rawoutput = net_connect.send_command("show interfaces status")
+    match10m = re.compile(r'-10\s')
+    seperatelines = rawoutput.split("\n")
+    for line in seperatelines:
+        mo = match10m.search(line)
+        if mo != None:
+            newline = line.split()
+            print(newline[0], newline[3], newline[4])
+
+if __name__ == "__main__":
+    Main()

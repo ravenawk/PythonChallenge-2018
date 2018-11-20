7602b608debc3274fdca2207dd20a939c5e4ca5a
William Martin <wpmartin@gmail.com>
2018-11-19 21:48:16 -0800

    Code cleanup

--- 11-November/only10m.py
+++ 11-November/only10m.py
@@ -8,11 +8,13 @@ import argparse
 import re
 
 
-def Main():
-    """Run if run as a program."""
+def main():
+    """Run if as run as a program. Parsing"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--device_os", type=str, default="cisco_ios",
                         help='Device OS default=Cisco IOS', metavar="")
+    parser.add_argument("-D", "--domain", type=str, default="ne.pvhmc.org",
+                        help='Domain, default is=ne.pvhmc.org', metavar="")
     parser.add_argument("-n", "--hostname", required=True,
                         help='Hostname of device', metavar="")
     parser.add_argument("-u", "--user", type=str, default="admin",
@@ -20,7 +22,7 @@ def Main():
     args = parser.parse_args()
 
     password = getpass.getpass()
-    hostname = args.hostname + ".ne.pvhmc.org"
+    hostname = args.hostname + "." + args.domain
     cisco = {'device_type': args.device_os,
              'ip': hostname,
              'username': args.user,
@@ -28,14 +30,14 @@ def Main():
              }
 
     net_connect = ConnectHandler(**cisco)
-    rawoutput = net_connect.send_command("show interfaces status")
+    raw_output = net_connect.send_command("show interfaces status")
     match10m = re.compile(r'-10\s')
-    seperatelines = rawoutput.split("\n")
-    for line in seperatelines:
+    for line in raw_output.split("\n"):
         mo = match10m.search(line)
-        if mo != None:
+        if mo is not None:
             newline = line.split()
             print(newline[0], newline[3], newline[4])
 
+
 if __name__ == "__main__":
-    Main()
+    main()
